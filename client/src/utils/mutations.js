a1c00e0271c718ae4098e38c1551cc986e35e0cd
Brahm V <brahmvanh@gmail.com>
2023-10-21 17:19:31 -0400

    Created mutations helper in client

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,18 @@
+import { gql } from '@apollo/client';
+
+export const CREATE_UNAVAILABLE_DATE = gql`
+	mutation createUnavailableDate($date: String!) {
+		createUnavailableDate(date: $date) {
+			_id
+			dateValue
+		}
+	}
+`;
+export const REMOVE_UNAVAILABLE_DATE = gql`
+	mutation removeUnavailableDate($date: String!) {
+		removeUnavailableDate(date: $date) {
+			_id
+			dateValue
+		}
+	}
+`;
