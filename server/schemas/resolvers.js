6074502e70091ddb17f1c51e5b7d43211b89a247
Brahm V <brahmvanh@gmail.com>
2023-10-21 14:29:25 -0400

    Created resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,8 +1,35 @@
 const { AuthenticationError } = require('apollo-server-express');
 
 const resolvers = {
-	Query: {},
-	Mutation: {},
+	Query: {
+		queryUnavailableDates: async () => {
+			try {
+				const dates = await UnavailableDates.find({});
+				if (!dates) {
+					throw new Error("Cannot find all dates in database");
+				} return dates;
+			} catch (err) {
+				return [{ message: "Error in queryUnavailableDates...", details: err.message }]
+			}
+		}
+	},
+	Mutation: {
+		createUnavailableDate: async (parent, {date}) => {
+			try {
+				if (!date) {
+					throw new Error("date object is undefined");
+				}
+				const unavailableDate = await UnavailableDates.create({dateValue: date});
+				
+				if (!unavailableDate) {
+					throw new Error("Could not create new date");
+				}
+				return unavailableDate;
+			} catch (err) {
+				return [{message: "Something went wrong creating a new unavailable date..."}]
+			}
+		}
+	},
 };
 
 module.exports = resolvers;
