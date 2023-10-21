9f43cbc30f652466a70b0034cef4be2945f73274
Brahm V <brahmvanh@gmail.com>
2023-10-21 17:28:35 -0400

    Got the app to start successfully. Imported my working calendars to test with back end queries/mutations

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Calendar from 'react-calendar';
+import AdminCalendar from './components/AdminCalendar';
+import {Button} from 'react-bootstrap';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [calendarVisible, setCalendarVisible] = useState(true);
+	const [adminCalendarVisible, setAdminCalendarVisible] = useState(false);
+
+	const handleButtonClick = (event) => {
+		event.preventDefault();
+		if (calendarVisible === true) {
+			setCalendarVisible(false);
+			setAdminCalendarVisible(true);
+		} else {
+			setCalendarVisible(true);
+			setAdminCalendarVisible(false);
+		}
+	};
+	return (
+		<div className='App'>
+			{calendarVisible ? <Calendar /> : <></>}
+			{adminCalendarVisible ? <AdminCalendar /> : <></>}
+      <Button onClick={handleButtonClick}>Click</Button>
+		</div>
+	);
 }
 
 export default App;
