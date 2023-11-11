5ccd49dcb055d73270760a7238b4b2ae98f0e1b5
Brahm Van Houzen <brahmvanh@gmail.com>
2023-11-11 13:17:07 -0500

    made navbar more response. Going to add animation to dropdown menu

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,40 +1,86 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Auth from '../../utils/auth';
+import { RxHamburgerMenu } from 'react-icons/rx';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
-
+import { Button } from 'react-bootstrap';
 
 function Navbar() {
+	const [mobileViewport, setMobileViewport] = useState(false);
+	const [showDropdownMenu, setShowDropdownMenu] = useState(false);
+	const [dropdownMenuDisplay, setDropdownMenuDisplay] = useState('none');
 
+	const isMediumViewport = () => {
+		return window.innerWidth < 766;
+	};
+
+	const toggleDropDown = () => {
+		showDropdownMenu ? setShowDropdownMenu(false) : setShowDropdownMenu(true);
+	};
+
+	useEffect(() => {
+		const mobile = isMediumViewport();
+		mobile ? setMobileViewport(true) : setMobileViewport(false);
+	}, []);
+
+	useEffect(() => {
+		showDropdownMenu ? setDropdownMenuDisplay('') : setDropdownMenuDisplay('none');
+	}, [showDropdownMenu]);
 
 	return (
-		<nav className='navbar navbar-expand navigation-clean navbar-light'>
-			<div className='navbar-inner-container container'>
-				<Link className='navbar-brand' to={'/'}>
-					Captains Rentals
-				</Link>
-				<div className='link-container'>
-					<Link to={'/'} className='navbar-link'>
-						Home
+		<>
+			<nav className='navbar navbar-expand navigation-clean navbar-light'>
+				<div className='navbar-inner-container container'>
+					<Link className='navbar-brand' to={'/'}>
+						Captains Rentals
 					</Link>
-					<Link to={'/about'} className='navbar-link'>
-						About Us
-					</Link>
-					<Link to={'/contact'} className='navbar-link'>
-						Contact
-					</Link>
-					{Auth.loggedIn() ? (
-						<Link to='/' onClick={() => Auth.logout()} className='navbar-link'>
-							Log Out
-						</Link>
+					{mobileViewport ? (
+						<button onClick={toggleDropDown}>
+							<RxHamburgerMenu />
+						</button>
 					) : (
-						<></>
+						<div className='link-container'>
+							<Link to={'/'} className='navbar-link'>
+								Home
+							</Link>
+							<Link to={'/about'} className='navbar-link'>
+								About Us
+							</Link>
+							<Link to={'/contact'} className='navbar-link'>
+								Contact
+							</Link>
+							{Auth.loggedIn() ? (
+								<Link to='/' onClick={() => Auth.logout()} className='navbar-link'>
+									Log Out
+								</Link>
+							) : (
+								<></>
+							)}
+						</div>
 					)}
 				</div>
+			</nav>
+			<div style={{ display: dropdownMenuDisplay }} className='dropdown'>
+				<Link to={'/'} className='navbar-link'>
+					Home
+				</Link>
+				<Link to={'/about'} className='navbar-link'>
+					About Us
+				</Link>
+				<Link to={'/contact'} className='navbar-link'>
+					Contact
+				</Link>
+				{Auth.loggedIn() ? (
+					<Link to='/' onClick={() => Auth.logout()} className='navbar-link'>
+						Log Out
+					</Link>
+				) : (
+					<></>
+				)}
 			</div>
-		</nav>
+		</>
 	);
 }
 
