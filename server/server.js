34b2230a91abb241e852ca8cfebae84d25489643
Brahm Van Houzen <brahmvanh@gmail.com>
2023-11-14 20:41:55 -0500

    set up an AWS S3 bucket to host my images to increase page loading speed. created awsConfig and configured

--- server/server.js
+++ server/server.js
@@ -17,7 +17,6 @@ const server = new ApolloServer({
 	context: authMiddleware,
 });
 
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
@@ -35,9 +34,7 @@ const startApolloServer = async (typeDefs, resolvers) => {
 	db.once('open', () => {
 		app.listen(PORT, () => {
 			console.log(`API server running on port ${PORT}!`);
-			console.log(
-				`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
-			);
+			console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 		});
 	});
 };
---
+++ server/utils/awsConfig.js
@@ -0,0 +1,9 @@
+import AWS from 'aws-sdk';
+
+AWS.config.update({
+	accessKeyId: process.env.S3_ACCESS_KEY,
+	secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+	region: 'us-east-2',
+});
+
+export const s3 = new AWS.S3();
